c5ed6b1161f790f094fa04178ce124e54932800c
CurlyHairedViz <viraj.shah1699@gmail.com>
2023-02-03 16:52:32 -0500

    Initial commit

---
+++ controllers/employers.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET employers index (the module home page) */
+router.get('/', (req, res) => {
+    res.render('employers/index', { title : 'Employer List' });
+});
+
+// make public
+module.exports = router;
